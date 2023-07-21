4369736627fa37c61efef0ca7eb560f05f8e8ff9
Lorena Roberta <lorenaroberta115@gmail.com>
2023-07-21 01:53:36 -0300

    inicio javascript do carrossel

--- script.js
+++ script.js
@@ -1,16 +1,59 @@
-const imgs = document.getElementById("img");
-const img = document.querySelectorAll("#img img");
+const galleryContainer = document.querySelector('.gallery-container');
+const galleryControlsContainer = document.querySelector('.gallery-controls');
+const galleryControls = ['previous', 'next'];
+const galleryItems = document.querySelectorAll('.gallery-item');
 
-let idx = 0;
-
-function carrossel(){
-  idx++;
-
-  if(idx > img.length - 1){
-    idx = 0;
+class Carousel {
+  
+  constructor(container, items, controls){
+    this.carouselContainer = container;
+    this.carouselControls = controls;
+    this.carouselArray = [...items];
   }
 
-  imgs.style.transform = 'translateX(${-idx * 500}px)';
+  updateGallery(){
+    this.carouselArray.forEach(el =>{
+      el.classList.remove('gallery-item1');
+      el.classList.remove('gallery-item2');
+      el.classList.remove('gallery-item3');
+      el.classList.remove('gallery-item4');
+      el.classList.remove('gallery-item5');
+    });
+
+    this.carouselArray.slice(0,5).forEach((el, i) =>{
+      el.classList.add(`gallery-item-${i+1}`);
+    });
+  }
+
+  setCurrentState(direction){
+    if(direction.className == 'gallery-controls-previous'){
+      this.carouselArray.unshift(this.carouselArray.pop());
+    }else{
+      this.carouselArray.push(this.carouselArray.shift());
+    }
+    this.updateGallery();
+  }
+
+  setControls(){
+    this.carouselControls.forEach(control =>{
+      galleryControlsContainer.appendChild(document.createElement('button')).className = `gallery-controls-${control}`;
+      document.querySelector(`.gallery-controls-${control}`).innerText = control;
+    });
+  }
+
+  useControls(){
+    const triggers = [...galleryControlsContainer.childNodes];
+    triggers.forEach(control => {
+      control.addEventListener('click', e =>{
+        e.preventDefault();
+        this.setCurrentState(control);
+      });
+    });
+  }
+
 }
 
-setInterval(carrossel, 1800);
+const exampleCarousel = new Carousel(galleryContainer, galleryItems, galleryControls);
+
+exampleCarousel.setControls();
+exampleCarousel.useCotrols();
